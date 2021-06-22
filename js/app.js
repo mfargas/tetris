636fefa599187c6b781c1ea46574f5c43f9d7b3f
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-06-22 13:07:03 -0700

    Create JavaScript constants and variables from doc

--- js/app.js
+++ js/app.js
@@ -1,2 +1,49 @@
 //tetris game
 //exercise for creating javascript apps and marrying them to a webpage
+
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const scoreBoard = document.querySelector('#score-board')
+    const startButton = document.querySelector('#start-button')
+    const width = 10;
+
+    const t1 = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
+
+    const t2 = [
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1]
+    ]
+
+    const t3 = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ]
+
+    const t4 = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ]
+
+    const t5 = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
+    ]
+
+    const tetriminoes = [ t1, t2, t3, t4, t5]
+
+
+})
