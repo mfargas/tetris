d9fa8111c15d94c711e07e021e79a622cf8401a2
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-07-13 23:48:57 -0700

    Update paths

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,3 @@
-const COLS = 10;
-const ROWS = 20;
-const BLOCK_SIZE = 30;
-
-export default { COLS, ROWS, BLOCK_SIZE };
+export const COLS = 10;
+export const ROWS = 20;
+export const BLOCK_SIZE = 30;
